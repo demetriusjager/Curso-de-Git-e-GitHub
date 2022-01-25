e4e704d2a1fb0b3d2aa6681291c8e6b2ada5fc46
demetriusjager <djager@bol.com.br>
2022-01-25 01:08:39 -0300

    commit inicial

---
+++ Exercício01.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Word");
+            Console.WriteLine("Olá meu nome é Demétrius Jager"); 
+
+
+            Console.ReadLine();
+        }
+    }
+}
